4292636f036ed8f39502edf46c9c06faa77052b7
Vitor Rocha <vitorlopeslrocha@gmail.com>
2023-03-10 15:45:32 -0300

    login page 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new()
+  end
+
+  def create
+    @user =  User.new(params[:user])
+    @user.save
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    @user.update(params[:user])
+  end
 end
