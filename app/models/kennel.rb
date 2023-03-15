a060137b47c0cb96f77a11c858c10808de8078c0
Daniel Nascimento <codingnascimento@gmail.com>
2023-03-15 01:57:42 +0100

    test

--- app/models/kennel.rb
+++ app/models/kennel.rb
@@ -4,5 +4,5 @@ class Kennel < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
   has_many_attached :photos
 
-  validates :name, :address, :phone_number, :email, :photos, presence: true
+  validates :name, :address, :phone_number, :email, presence: true
 end
