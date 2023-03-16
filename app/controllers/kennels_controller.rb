223073735bc2b9c4670a69bba4161567ba18ff7b
Vitor Rocha <vitorlopeslrocha@gmail.com>
2023-03-16 15:10:55 -0300

    New kennel page

--- app/controllers/kennels_controller.rb
+++ app/controllers/kennels_controller.rb
@@ -4,6 +4,35 @@ class KennelsController < ApplicationController
   end
 
   def show
-   @kennel = Kennel.find(params[:id])
+    @kennel = Kennel.find(params[:id])
+  end
+
+  def new
+    @kennel = Kennel.new
+  end
+
+  def create
+    @kennel = Kennel.new(kennel_params)
+    if @kennel.save
+      redirect_to pet_path(@kennel)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  def edit
+    @kennel = Kennel.find(params[:id])
+  end
+
+  def update
+    @kennel = Kennel.find(params[:id])
+    @kennel.update(kennel_params)
+  end
+
+  private
+
+  def kennel_params
+    params.require(:kennel).permit(:name, :address, :phone_number, :email, :photos)
   end
 end
