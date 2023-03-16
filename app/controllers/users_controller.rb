e80bd5ed455b29b7a8b009cd6f013b66b5e75ebe
Vitor Rocha <vitorlopeslrocha@gmail.com>
2023-03-16 10:35:44 -0300

    New pet page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
+    @user = User.find(params[:id])
   end
   def update
     @user = User.find(params[:id])
